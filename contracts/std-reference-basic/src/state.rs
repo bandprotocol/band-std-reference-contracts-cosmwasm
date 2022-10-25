ea7499427816f0b617b0fbe8249e11206e4ba8f1
warittornc <69615455+warittornc@users.noreply.github.com>
2022-10-25 17:00:46 +0700

    update contract to better fit cosmwasm format

--- contracts/std-reference-basic/src/state.rs
+++
@@ -1,8 +0,0 @@
-use cosmwasm_std::Addr;
-use cw_storage_plus::{Item, Map};
-
-use crate::struct_types::{Config, RefData};
-
-pub const CONFIG: Item<Config> = Item::new("config");
-pub const RELAYERS: Map<&Addr, bool> = Map::new("relayers");
-pub const REFDATA: Map<&str, RefData> = Map::new("refdata");
